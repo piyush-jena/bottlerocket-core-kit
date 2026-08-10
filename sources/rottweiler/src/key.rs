308bd099ef13095ef6e44d96d0dcfff72c5125a3
Piyush Jena <jepiyush@amazon.com>
2026-08-10 05:54:46 +0000

    rottweiler: add delete-key subcommand
    Signed-off-by: Piyush Jena <jepiyush@amazon.com>

--- sources/rottweiler/src/key.rs
+++ sources/rottweiler/src/key.rs
@@ -48,6 +48,28 @@ pub fn generate(key_id: String) -> Result<()> {
     Ok(())
 }
 
+/// Delete a sealed key from the keystore.
+pub fn delete(key_id: String) -> Result<()> {
+    if !key_id
+        .chars()
+        .all(|c| c.is_alphanumeric() || c == '-' || c == '_')
+    {
+        whatever!("key_id must contain only alphanumerics, dashes, and underscores");
+    }
+
+    let key_path = keystore_dir().join(&key_id);
+
+    // Skip deletion if key doesn't exists
+    if !key_path.exists() {
+        return Ok(());
+    }
+
+    fs::remove_file(&key_path)
+        .with_whatever_context(|_| format!("failed to delete key '{}'", key_path.display()))?;
+
+    Ok(())
+}
+
 /// Load and decrypt a TPM2-encrypted key from the keystore
 pub fn load(key_id: String) -> Result<Zeroizing<Vec<u8>>> {
     let key_path = PathBuf::from(KEYSTORE).join(&key_id);
