8f891bbc54a1c7e0ded6f0c0e455373ff4439cd8
Piyush Jena <jepiyush@amazon.com>
2026-08-10 06:15:16 +0000

    rottweiler: add support for tmpfs keys
    Signed-off-by: Piyush Jena <jepiyush@amazon.com>

--- sources/rottweiler/src/key.rs
+++ sources/rottweiler/src/key.rs
@@ -1,4 +1,5 @@
 use snafu::prelude::*;
+use std::env;
 use std::fs;
 use std::io::Read;
 use std::path::PathBuf;
@@ -9,8 +10,24 @@ use crate::system;
 type Result<T> = std::result::Result<T, snafu::Whatever>;
 
 const DEV_RANDOM: &str = "/dev/random";
-const KEYSTORE: &str = "/.bottlerocket/keystore";
+const KEYSTORE_PERMANENT: &str = "/.bottlerocket/keystore";
+const KEYSTORE_EPHEMERAL: &str = "/run/rottweiler";
+const EPHEMERAL_ENCRYPTION_KEYS_ENVVAR: &str = "EPHEMERAL_ENCRYPTION_KEYS";
 const KEY_SIZE: usize = 64;
+
+fn keystore_dir() -> PathBuf {
+    let ephemeral_encryption_keys_enabled = env::var(EPHEMERAL_ENCRYPTION_KEYS_ENVVAR)
+        .ok()
+        .and_then(|v| v.parse().ok())
+        .unwrap_or(false);
+
+    // If ephemeral encryption keys are enabled, we store the keys in tmpfs rather than the disk
+    if ephemeral_encryption_keys_enabled {
+        PathBuf::from(KEYSTORE_EPHEMERAL)
+    } else {
+        PathBuf::from(KEYSTORE_PERMANENT)
+    }
+}
 
 /// Generate a random encryption key and encrypt it with TPM2 PCRs 7+14
 pub fn generate(key_id: String) -> Result<()> {
@@ -21,7 +38,8 @@ pub fn generate(key_id: String) -> Result<()> {
         whatever!("key_id must contain only alphanumerics, dashes, and underscores");
     }
 
-    let key_path = PathBuf::from(KEYSTORE).join(&key_id);
+    let keystore = keystore_dir();
+    let key_path = keystore.join(&key_id);
 
     // Skip generation if key already exists
     if key_path.exists() {
@@ -39,8 +57,12 @@ pub fn generate(key_id: String) -> Result<()> {
 
     let encrypted = system::systemd_creds_encrypt(&key_id, &random_bytes)?;
 
-    fs::create_dir_all(KEYSTORE)
-        .with_whatever_context(|_| format!("failed to create keystore directory '{}'", KEYSTORE))?;
+    fs::create_dir_all(&keystore).with_whatever_context(|_| {
+        format!(
+            "failed to create keystore directory '{}'",
+            keystore.display()
+        )
+    })?;
 
     fs::write(&key_path, encrypted)
         .with_whatever_context(|_| format!("failed to write key to '{}'", key_path.display()))?;
@@ -72,7 +94,7 @@ pub fn delete(key_id: String) -> Result<()> {
 
 /// Load and decrypt a TPM2-encrypted key from the keystore
 pub fn load(key_id: String) -> Result<Zeroizing<Vec<u8>>> {
-    let key_path = PathBuf::from(KEYSTORE).join(&key_id);
+    let key_path = keystore_dir().join(&key_id);
 
     let encrypted = fs::read(&key_path)
         .with_whatever_context(|_| format!("failed to read key from '{}'", key_path.display()))?;
